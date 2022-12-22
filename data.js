91e0e7de6421eb163ce28ff51ee293f238b14c8d
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-12-22 13:45:34 -0500

    Modificacion

--- data.js
+++ data.js
@@ -24,20 +24,20 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.11759149530770685,
-        "pitch": 0.19302182084389408,
-        "fov": 1.4411046624168176
+        "yaw": -0.12351560860267874,
+        "pitch": 0.16725612982993887,
+        "fov": 1.4134061960355204
       },
       "linkHotspots": [
         {
-          "yaw": 0.765190736741669,
-          "pitch": 0.35053075590303706,
+          "yaw": 0.8037670841369984,
+          "pitch": 0.3102348539533395,
           "rotation": 0.7853981633974483,
           "target": "2-cama"
         },
         {
-          "yaw": 0.0455130180300074,
-          "pitch": 0.24236496625551673,
+          "yaw": 0.02917094492979011,
+          "pitch": 0.198229448844355,
           "rotation": 5.497787143782138,
           "target": "1-bao"
         }
@@ -68,14 +68,14 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0.5148035584227753,
-        "pitch": 0.31799905689117125,
-        "fov": 1.4436873923031792
+        "yaw": -0.18632419184306315,
+        "pitch": 0.352891742595709,
+        "fov": 1.4134061960355204
       },
       "linkHotspots": [
         {
-          "yaw": -2.0658443019213877,
-          "pitch": 0.18226907306880058,
+          "yaw": -2.703562547314391,
+          "pitch": 0.06173273977495519,
           "rotation": 5.497787143782138,
           "target": "0-entrada-y-cocina"
         }
@@ -106,31 +106,31 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.1668922661315193,
-        "pitch": 0.4526821248712878,
-        "fov": 1.4436873923031792
+        "yaw": 0.35577865521423924,
+        "pitch": 0.31061852682702984,
+        "fov": 1.4134061960355204
       },
       "linkHotspots": [
         {
-          "yaw": -2.8858557626313743,
-          "pitch": 0.1605279863984208,
-          "rotation": 11.780972450961727,
-          "target": "0-entrada-y-cocina"
-        },
-        {
-          "yaw": -1.9535206841353627,
-          "pitch": 0.14066202821306284,
+          "yaw": -1.6305065560288874,
+          "pitch": 0.2222881108550201,
           "rotation": 0.7853981633974483,
           "target": "1-bao"
+        },
+        {
+          "yaw": -2.968697013275696,
+          "pitch": 0.24559853685590127,
+          "rotation": 0,
+          "target": "0-entrada-y-cocina"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Apartamento M",
+  "name": "LIVING 35 Apto M",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": false,
+    "autorotateEnabled": true,
     "fullscreenButton": false,
     "viewControlButtons": false
   }
